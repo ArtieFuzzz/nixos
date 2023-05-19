f727b445991959d4bb50990cb61c57532f880694
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-05-19 21:55:58 +1000

    fix: Fix import pt.2

--- base.nix
+++ base.nix
@@ -1,18 +1,19 @@
-{
-  environment.systemPackages = [
-    pkgs.btop
-    pkgs.neofetch
-  ];
-
-  system.autoUpgrade = {
-    enable = true;
-  };
-
-  networking.nameservers = [ "1.1.1.1" "9.9.9.9" ];
-
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-}
+{ pkgs }:
+  {
+    environment.systemPackages = [
+      pkgs.btop
+      pkgs.neofetch
+    ];
+  
+    system.autoUpgrade = {
+      enable = true;
+    };
+  
+    networking.nameservers = [ "1.1.1.1" "9.9.9.9" ];
+  
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  }
