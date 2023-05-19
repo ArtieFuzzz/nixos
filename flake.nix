93e1ae2cb8667f6157680cddba30340dcd271b90
ArtieFuzzz3 <48877462+ArtieFuzzz@users.noreply.github.com>
2023-05-19 06:20:02 +0000

    chore: Update config

--- flake.nix
+++ flake.nix
@@ -1,11 +1,14 @@
 {
-  description = "A very basic flake";
+  description = "NixOS configurations";
 
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    packages.x86_64-linux.gh = nixpkgs.gh;
+    packages.x86_64-linux.git = nixpkgs.git;
+    packages.x86_64-linux.starship = nixpkgs.starship;
+    packages.x86_64-linux.zsh-autosuggestions = nixpkgs.zsh-autosuggestions;
+    packages.x86_64-linux.zoxide = nixpkgs.zoxide;
+    packages.x86_64-linux.btop = nixpkgs.btop;
+    #packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    #packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
   };
 }
