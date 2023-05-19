0bb3b07824da25e9bd3eb4eda672fba4eae8be27
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-05-19 21:45:51 +1000

    fix: Incorrectly typed name

--- flake.nix
+++ flake.nix
@@ -6,12 +6,12 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkg }:
+  outputs = { self, nixpkgs }:
     let
       systems = ["x86_64-linux-gnu"];
     in
   {
-    nixosConfigurations.iridium = nixpkg.lib.nixosSystem {
+    nixosConfigurations.iridium = nixpkgs.lib.nixosSystem {
       inherit systems;
       modules = [
         ./configuration.nix
